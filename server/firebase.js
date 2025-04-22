564adc3321dc07d59a04823afd1c9cce52dad2fb
Dirceu Garcia <dirceu.garcia@gmail.com>
2025-04-21 23:00:59 -0300

    Mudando algumas variáveis de ambiente (21/04)

--- server/firebase.js
+++ server/firebase.js
@@ -1,9 +1,26 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../securityNotes/role-eventos-itinerantes-firebase-adminsdk-fbsvc-8647312e1a.json'); // Substitua pelo caminho correto do seu arquivo de chave privada
+//const serviceAccount = require('../securityNotes/role-eventos-itinerantes-firebase-adminsdk-fbsvc-8647312e1a.json'); // Substitua pelo caminho correto do seu arquivo de chave privada
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined, // Trata quebras de linha
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://role-eventos-itinerantes.firebaseio.com'
 });
 
-module.exports = admin;
+const auth = admin.auth();
+const firestore = admin.firestore();
+
+
+module.exports = {admin, auth, firestore};
