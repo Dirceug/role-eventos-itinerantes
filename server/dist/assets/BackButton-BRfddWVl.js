08f1a32fc19511e6d8fc8203ab3c4b67f59ae3e1
Dirceu Garcia <dirceu.garcia@gmail.com>
2025-05-05 18:50:59 -0300

    tudo rodando na minha maquina com build (05/05)

---
+++ server/dist/assets/BackButton-BRfddWVl.js
@@ -0,0 +1,11 @@
+import { u as useNavigate, j as jsxRuntimeExports } from "./index-BYsE5TuP.js";
+const BackButton = () => {
+  const navigate = useNavigate();
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("button", { onClick: handleBackClick, className: "back-button no-hover", children: "←" });
+};
+export {
+  BackButton as default
+};
---
+++ server/dist/assets/Usuarios-DylUfZZv.js
@@ -0,0 +1,12 @@
+import { j as jsxRuntimeExports } from "./index-BYsE5TuP.js";
+import "./index-9ae71ce3-D9oYUShw.js";
+/* empty css                        */
+import "./Navbar-B2cfVznA.js";
+import ListaEventos from "./ListaEventos-CCW-4lWR.js";
+import "./eventoBase-jXVQ2lm8.js";
+function Usuarios() {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", { children: /* @__PURE__ */ jsxRuntimeExports.jsx(ListaEventos, {}) });
+}
+export {
+  Usuarios as default
+};
